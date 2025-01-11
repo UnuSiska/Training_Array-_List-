7f437403fc56b00366bc23a94bf3283a35b8f815
UnuSiska <ksapoval00@gmail.com>
2025-01-11 20:11:39 +0300

    Сделал первые 5 заданий из 1 пункта на сайте, остановился на 2 пункте

---
+++ A_Project_For_Training/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Project_For_Training
+{
+    internal class Program
+    {
+        //Сделать массив, полем класса, НЕ ЗАБУДЬ!!! ОЛУХ
+        private void IteratingThroughArrays()
+        {
+            int[] array = {1, 2, 3, 4};
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == array.Length - 1)
+                {
+                    break;
+                }else Console.WriteLine(array[i]);
+            }
+            //Делал на сайте, ссылка: http://csharpbook.sakutin.ru/задачи-на-массивы/
+        }
+        private void AddingAndRemovingArrayElements()
+        {
+            int[] array = { 1, 2, 3, 4 };
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.AddingAndRemovingArrayElements();
+        }
+    }
+}
